638964ac4c0ec6d1b618fad206836cbe3976b2b6
palat2000 <palat.tungsantitham@gmail.com>
2024-02-02 22:10:27 +0700

    remove field bgcolor

--- src/model/authentication.model.js
+++ src/model/authentication.model.js
@@ -9,10 +9,6 @@ const authenticationSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  backgroundColor: {
-    type: String,
-    required: true,
-  },
   password: {
     type: String,
     required: true,
